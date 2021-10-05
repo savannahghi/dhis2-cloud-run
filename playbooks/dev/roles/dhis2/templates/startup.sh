e9633c989209ab780c8f9103ff5530891ae24625
saladgg <saladguyo60@gmail.com>
2021-10-05 10:50:34 +0300

    fix: setup separate playbook environment for development and deployment

--- playbooks/dev/roles/dhis2/templates/startup.sh
+++ playbooks/dev/roles/dhis2/templates/startup.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 set -e
 
-if [[ `id -nu` != "{{dhis_user}}" ]];then
-   echo "Not running as {{dhis_user}}, exiting.."
+if [[ `id -nu` != "{{test_dhis_user}}" ]];then
+   echo "Not running as {{test_dhis_user}}, exiting.."
    exit 1
 fi
 
-/home/{{dhis_user}}/{{dhis_user}}/bin/setenv.sh
+/home/{{test_dhis_user}}/{{test_dhis_user}}/bin/setenv.sh
 
-export CATALINA_BASE="/home/{{dhis_user}}/{{dhis_user}}"
+export CATALINA_BASE="/home/{{test_dhis_user}}/{{test_dhis_user}}"
 /usr/share/tomcat9/bin/startup.sh
 echo "Tomcat started"
