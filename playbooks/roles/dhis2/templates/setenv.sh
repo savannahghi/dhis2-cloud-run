13e48b22cdb3c08e89131629ecd8fb233d404b20
saladgg <saladguyo60@gmail.com>
2022-09-07 10:44:32 +0300

    chore: update playbook to use pg_14 and dhis_2.36.11.1

--- playbooks/roles/dhis2/templates/setenv.sh
+++ playbooks/roles/dhis2/templates/setenv.sh
@@ -3,7 +3,7 @@
 CATALINA_HOME=/usr/share/tomcat9
 
 export JAVA_HOME='/usr/lib/jvm/java-11-openjdk-amd64/'
-export JAVA_OPTS='-Xmx1000m -Xms1000m'
+export JAVA_OPTS='-Xmx5000m -Xms3000m'
 export DHIS2_HOME='/home/{{dhis_user}}/config'
 
 # The first existing directory is used for JAVA_HOME (if JAVA_HOME is not
